ec75279dfb83abaabb76b8b67cf9ccdcf47e69b5
Alisson <alissonpintor@gmail.com>
2024-06-21 16:37:33 -0400

    Adicionado a estrutura das pastas

---
+++ lib/utils/consts.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const CARD_BACKGROUND_COLOR = Color(0xFF21222D);
+const PRIMARY_COLOR = Color(0xFF2697FF);
+const SECONDARY_COLOR = Color(0xFFFFFFFF);
+const BACKGROUND_COLOR = Color(0xFF15131C);
+const SELECTION_COLOR = Color(0xFF88B2AC);
+
+const DEFAULT_PADDING = 20.0;
